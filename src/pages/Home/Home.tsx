b5c1009888a59c6121dbece7aaaa89e956abb43b
pratik860s <pratiksharma860@gmail.com>
2024-08-13 23:13:59 +0530

    changes

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,9 +4,6 @@ import Footer from "../../components/Footer";
 import Products from "../../components/Products/Products";
 import { Ecosystem } from "../../components/Ecosystem/Ecosystem";
 import Features from "../../components/Features/Features";
-import StreetLight from "../../components/StreetLight";
-import Root from "../../components/Root/Root";
-// import Ecosystem from "../../components/Products/Products";
 
 function Home() {
   return (
@@ -14,7 +11,6 @@ function Home() {
       <Navbar />
       <HeroSection />
       <Ecosystem />
-      {/* <StreetLight/> */}
       <Products />
       <Features />
       <Footer />
